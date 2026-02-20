668cec4e77519464b063cf786b83ea54cc016d21
shivamsinghrajpoot67-creator <shivamsinghrajpoot67@gmail.com>
2026-02-20 07:42:54 +0530

    Fix syntax error in calculate function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function calculate(operator){
-  let num1 = Number(document.getElementById('num1').value;
-  let num2 = Number(document.getElementById('num2').value;
+  let num1 = Number(document.getElementById('num1').value);
+  let num2 = Number(document.getElementById('num2').value);
   let result;
 
 if(operator === "+") {
