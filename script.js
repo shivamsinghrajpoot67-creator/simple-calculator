5a828bf887bc30780185d9d61aa6414b314963b6
shivamsinghrajpoot67-creator <shivamsinghrajpoot67@gmail.com>
2026-02-20 07:34:59 +0530

    Implement basic calculator functionality
    Fix calculation logic and update result display.

---
+++ script.js
@@ -0,0 +1,18 @@
+function calculate(operator){
+  let num1 = Number(document.getElementById('num1').value;
+  let num2 = Number(document.getElementById('num2').value;
+  let result;
+
+if(operator === "+") {
+  result = num1 + num2;
+} else if ( operator === "-") {
+  result = num1 - num2;
+} else if ( operator === "*") {
+  
+} else if (operator === "/") {
+  result = num1 * num2;
+} else if (operator === "%") {
+  result = num1 / num2;
+}
+
+document.getElementById("result).text = "Result :" + " " + result;
