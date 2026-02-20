7c0c66a610d6085b42d1e54fa1f7eee74289a4f4
shivamsinghrajpoot67-creator <shivamsinghrajpoot67@gmail.com>
2026-02-20 07:53:13 +0530

    Refactor calculate function for improved readability

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-function calculate(operator){
+function calculate(operator) {
   let num1 = Number(document.getElementById('num1').value);
   let num2 = Number(document.getElementById('num2').value);
   let result;
 
-if(operator === "+") {
-  result = num1 + num2;
-} else if ( operator === "-") {
-  result = num1 - num2;
-} else if ( operator === "*") {
-  result = num1 * num2;
-} else if (operator === "/") {
-  result = num1 / num2;
-} else if (operator === "%") {
-  result = num1 % num2;
-}
+  if (operator === "+") {
+    result = num1 + num2;
+  } 
+  else if (operator === "-") {
+    result = num1 - num2;
+  } 
+  else if (operator === "*") {
+    result = num1 * num2;
+  } 
+  else if (operator === "/") {
+    result = num1 / num2;
+  } 
+  else if (operator === "%") {
+    result = num1 % num2;
+  }
 
-document.getElementById("result").innerText = "Result :" + " " + result;
+  document.getElementById("result").innerText = "Result : " + result;
 }
